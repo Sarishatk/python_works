3257303b149722ee80f5482e8d1fba53e706dc9a
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-28 09:48:04 +0530

    chck num is positive negative or zero

--- conditional_statements/chck_num.py
+++ conditional_statements/chck_num.py
@@ -12,8 +12,22 @@
 
 # 2.write a progarm that display last digit of a number is odd or even
 
-num = int(input("Enter a number : "))
-if num%2 == 0:
-    print("number is even")
+# num = int(input("Enter a number : "))
+# if num%2 == 0:
+#     print("number is even")
+# else:
+#     print("number id odd")
+
+# ---------------------------------------------
+
+# 3.check the number is positive negative zero or invalid
+
+num = int(input("enter a number : "))
+if num>0:
+    print("number is positive")
+elif num==0:
+    print("number is zero")
+elif num<0:
+    print("number is negative")
 else:
-    print("number id odd")
+    print("you are a fool!!")
