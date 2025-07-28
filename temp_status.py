c71d6c7914f1580b2cbd999e4db3cc8928569097
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-28 10:07:43 +0530

    temperature based calculATIONS

--- temp_status.py
+++ temp_status.py
@@ -1,12 +1,30 @@
-maths = int(input("Enter you are scored mark of maths : "))
-java = int(input("Enter you are scored mark of java : "))
-os = int(input("Enter you are scored mark of os : "))
-total_scored = maths + java + os
-if total_scored>140:
-    print("good")
-elif total_scored>130 and total_scored<140:
-    print("average")
-elif total_scored<130:
-    print("poor")
-else:
-    print("you are a fool!!")
+# maths = int(input("Enter you are scored mark of maths : "))
+# java = int(input("Enter you are scored mark of java : "))
+# os = int(input("Enter you are scored mark of os : "))
+# total_scored = maths + java + os
+# if total_scored>140:
+#     print("good")
+# elif total_scored>130 and total_scored<140:
+#     print("average")
+# elif total_scored<130:
+#     print("poor")
+# else:
+#     print("you are a fool!!")
+
+
+# ====================================================================
+
+
+# 2. 
+
+temperature = int(input("enter the temperature in degree celsius : "))
+if temperature>10 and temperature<20:
+    print("cold")
+elif temperature>20 and temperature<25:
+    print("pleasent|mild")
+elif temperature>25 and temperature<30:
+    print("warm")
+elif temperature>30 and temperature<35:
+    print("hot")
+elif temperature>35:
+    print("scorching | veryhot")
