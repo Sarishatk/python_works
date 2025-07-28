479d9ec987370e62c236a44afd08820eaba070ee
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-28 20:51:10 +0530

    leap year or not

--- conditional_statements/greatest_of_two_numbers.py
+++ conditional_statements/greatest_of_two_numbers.py
@@ -12,12 +12,22 @@
 
 # greatest among three numbers
 
-num1 = int(input("Enter first number : "))
-num2 = int(input("Enter second number : "))
-num3 = int(input("Enter third number : "))
-if num1>num2 and num1>num3:
-    print(num1 ,"is greater")
-elif num2>num1 and num2>num3:
-    print(num2,"is greater")
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter second number : "))
+# num3 = int(input("Enter third number : "))
+# if num1>num2 and num1>num3:
+#     print(num1 ,"is greater")
+# elif num2>num1 and num2>num3:
+#     print(num2,"is greater")
+# else:
+#     print(num3,"is greater")
+
+
+# =======================================================
+
+# 3. chechk the year is leap year or not
+year = int(input("enter a year : "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("it is a leap year")
 else:
-    print(num3,"is greater")
+    print("not a leap year")
