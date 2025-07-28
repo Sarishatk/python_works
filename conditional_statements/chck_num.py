308eb584bc8a0014d4bae5fc2faa8371042c9b40
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-28 09:33:45 +0530

    programs

---
+++ conditional_statements/chck_num.py
@@ -0,0 +1,19 @@
+
+#1.  check the number is positive or negative
+
+# num = int(input("enter a number : "))
+# if num>0:
+#     print("number is positive")
+# else:
+#     print("number is negative")
+
+# --------------------------------------------
+
+
+# 2.write a progarm that display last digit of a number is odd or even
+
+num = int(input("Enter a number : "))
+if num%2 == 0:
+    print("number is even")
+else:
+    print("number id odd")
