72d23ac556bd980c2500fcaf4e4679d3f2d00957
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-29 10:11:30 +0530

    pattern print

--- hello.py
+++ hello.py
@@ -3,6 +3,14 @@
 #     print("hello world")
 #     counter+=1
 
-for i in range(1,51):
-    print(i)
+# for i in range(1,51):
+#     print(i)
 
+
+i = 0
+while i<10:
+ if i ==9:
+  continue
+ else:
+  print(i)
+  i+=1
