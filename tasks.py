f79143b2a18ed0991feb64ee04c2f8cd069e0ae3
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-29 13:41:48 +0530

    diaplay all leap years from 1879 to 2026

--- tasks.py
+++ tasks.py
@@ -13,27 +13,50 @@
 #     start+=1
 
 
-
+# =====================================================
 
 #q2 => display all even number from 1 to 100
 
 # for i in range(0,101,2):
 #     print(i)
 
-start = 1
-stop = 100
+# start = 1
+# stop = 100
 
-while start <= stop:
-    if start % 2 == 0:
-        print(start)
-    start += 1
+# while start <= stop:
+#     if start % 2 == 0:
+#         print(start)
+#     start += 1
 
+
+# ========================================================
 
 
 #q3 => display all ood numbers from 1 to 100
+# start = 1
+# stop = 100
+
+
+# while start <= stop:
+#     if start % 2 != 0:
+#         print(start)
+#     start += 1
+# ============================================================
+
 
 # q4 => display all century years from 1879 to 2026
 
+
+
 # q5 => dispaly all non century years from 1879 to 2026
 
+
+
 # q6 => diaplay all leap years from 1879 to 2026
+start = 1879
+stop = 2026
+
+while start < stop:
+    if (start % 4 == 0 and start % 100 != 0) or (start % 400 == 0):
+        print(start)
+    start += 1
