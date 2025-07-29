bdbd0caf8902f42ae952e91a2b1fa082556ac9ca
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-29 09:08:16 +0530

    find the year ending with 2 zeros or not

--- pingpong.py
+++ pingpong.py
@@ -1,7 +1,26 @@
+
+# 1.check the number is ping,pomg or pingpong
 num = int(input("enter a number : "))
+
 if num%15==0 :
+
     print("PINGPONG")
+
 elif num%3==0:
+
     print("PING")
+
 elif num%5==0:
+
     print("PONG")
+
+
+
+# 2.
+
+year = int(input("enter a year : "))
+
+if year%100 ==0:
+    print("ending with two zeros")
+else:
+    print("non centdury year")
