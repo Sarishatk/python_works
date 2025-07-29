b7f8d20df277a428cc1b478145edf4e160764895
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-29 10:00:41 +0530

    print 1 to 50 using range

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-counter = 1
-while counter<=5:
-    print("hello world")
-    counter+=1
+# counter = 1
+# while counter<=5:
+#     print("hello world")
+#     counter+=1
+
+for i in range(1,51):
+    print(i)
+
