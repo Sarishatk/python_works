f50037cee09e5f5f2755edaa66754ec23497415b
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-29 13:15:59 +0530

    read start and stop display all numbers from start to stop

---
+++ tasks.py
@@ -0,0 +1,20 @@
+# q1 => read start and stop display all numbers from start to stop
+
+start = int(input("Enetr starting number : "))
+stop =  int(input("Enetr stoping number : "))
+for i in range(start,stop+1):
+    print(i)
+
+
+
+
+
+#q2 => display all even number from 1 to 100
+
+#q3 => display all ood numbers from 1 to 100
+
+# q4 => display all century years from 1879 to 2026
+
+# q5 => dispaly all non century years from 1879 to 2026
+
+# q6 => diaplay all leap years from 1879 to 2026
