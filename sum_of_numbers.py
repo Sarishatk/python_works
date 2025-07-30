41dc366bc135709918c5d5138871227a2d287c15
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-30 09:48:54 +0530

    task

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -1,7 +1,17 @@
-num = 234
+
+# set number 
+# set sum 
+# loop
+  #extract last digit
+  #add last digit with sum
+  # remove last digit from number
+# print sum
+   
+
+num = 5643
 sum = 0
-while (num!=0):
-    last_digit = num % 10 
-    sum = sum + last_digit
+while num!=0:
+    last_digit = num % 10
+    sum+=last_digit
     num = num // 10
 print(sum)
