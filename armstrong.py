81e8c68eb0d57182bfb195ec319eacceba02f2f1
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-30 10:19:07 +0530

    chck the number is armstrong or not

---
+++ armstrong.py
@@ -0,0 +1,14 @@
+# chck the number is armstrong or not
+
+num = int(input("enter a armstrong number :"))
+original = num
+sum = 0
+while num!=0:
+    digit = num % 10
+    cube = digit**3
+    sum+=cube
+    num = num // 10
+if sum == original :
+    print(sum,"is a armstrong number")
+else:
+    print(sum,"not a armstrong number")
