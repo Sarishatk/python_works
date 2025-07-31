45fc4d0a4ee79328303739220ac76011053c511a
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-31 10:31:40 +0530

    gfg

--- for_loop.py/gcd_of_two_numbers.py
+++ for_loop.py/gcd_of_two_numbers.py
@@ -2,7 +2,9 @@
 
 first_no = int( input("Enter first number : "))
 last_no = int( input("Enter last number : "))
+gcd = 1
 limit = min(first_no, last_no)
 for i in range(1,limit+1):
     if first_no % i == 0 and last_no % i == 0:
-        print(i)
+       gcd=i
+print(gcd)
