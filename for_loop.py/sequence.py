ccd56cd8829a31c82077c099e40c161ac03aff7c
Sarisha.tk <sarishakrishna58@gmail.com>
2025-07-31 08:52:34 +0530

    print number upto user need

---
+++ for_loop.py/sequence.py
@@ -0,0 +1,8 @@
+
+# 1.print number upto user need
+
+
+lower_limit = int(input("enter lower limit : "))
+upper_limit = int(input("enter lower limit : "))
+for i in range(lower_limit,upper_limit):
+    print(i)
