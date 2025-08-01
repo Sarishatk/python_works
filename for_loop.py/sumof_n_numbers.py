4eacb0d6edb83c9e857a23e80e2934f3cc45695c
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-01 10:18:34 +0530

    Print All Even Numbers Between 1 and N

--- for_loop.py/sumof_n_numbers.py
+++ for_loop.py/sumof_n_numbers.py
@@ -7,11 +7,15 @@
 # print(sum)
 
 # 2.Sum of Squares of First N Natural Numbers
-num = int(input("enter the number : "))
-sum = 0
-for i in range(1,num+1):
-    squre = i**2
-    sum+=squre
-print(sum)
+# num = int(input("enter the number : "))
+# sum = 0
+# for i in range(1,num+1):
+#     squre = i**2
+#     sum+=squre
+# print(sum)
 
  
+# 3.Print All Even Numbers Between 1 and N
+num = int(input("enter the number : "))
+for i in range(0,num+1,2):
+    print(i)
