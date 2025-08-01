87abe20f37b884745790b272e8b7f8066ae583cb
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-01 09:38:09 +0530

    import random number

--- for_loop.py/factoral.py
+++ for_loop.py/factoral.py
@@ -1,5 +1,17 @@
-num = int(input("enter a number : "))
-factorial = 1
-for i in range(1,num+1):
-    factorial = factorial*i
-print(factorial)
+# num = int(input("enter a number : "))
+# factorial = 1
+# for i in range(1,num+1):
+#     factorial = factorial*i
+# print(factorial)
+
+
+from random import randint
+target = randint(1,11)
+count = 0
+while True:
+    num = int(input("guess the number : "))
+    count+=1
+    if num == target:
+        print(count,"times")
+        print("congarts")
+        
