446327962859294a492624d7b76477a1549daa40
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-01 10:14:50 +0530

    Sum of Squares of First N Natural Numbers

--- for_loop.py/sumof_n_numbers.py
+++ for_loop.py/sumof_n_numbers.py
@@ -1,7 +1,17 @@
-# 1.	Sum of First N Natural Numbers
+# 1.Sum of First N Natural Numbers
 
+# num = int(input("enter the number : "))
+# sum = 0
+# for i in range(1,num+1):
+#     sum+=i
+# print(sum)
+
+# 2.Sum of Squares of First N Natural Numbers
 num = int(input("enter the number : "))
 sum = 0
 for i in range(1,num+1):
-    sum+=i
+    squre = i**2
+    sum+=squre
 print(sum)
+
+ 
