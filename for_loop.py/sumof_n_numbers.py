325cb9174077f2cacf19de16dba1d4c1ad6f3342
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-03 19:57:26 +0530

    2nd method of find reverse of a string

--- for_loop.py/sumof_n_numbers.py
+++ for_loop.py/sumof_n_numbers.py
@@ -1,32 +1,41 @@
 # 1.Sum of First N Natural Numbers
 
-# num = int(input("enter the number : "))
-# sum = 0
-# for i in range(1,num+1):
-#     sum+=i
-# print(sum)
+num = int(input("enter the number : "))
+sum = 0
+for i in range(1,num+1):
+    sum+=i
+print(sum)
 
 # 2.Sum of Squares of First N Natural Numbers
-# num = int(input("enter the number : "))
-# sum = 0
-# for i in range(1,num+1):
-#     squre = i**2
-#     sum+=squre
-# print(sum)
+num = int(input("enter the number : "))
+sum = 0
+for i in range(1,num+1):
+    squre = i**2
+    sum+=squre
+print(sum)
 
  
 # 3.Print All Even Numbers Between 1 and N
-# num = int(input("enter the number : "))
-# for i in range(0,num+1,2):
-#     print(i)
+num = int(input("enter the number : "))
+for i in range(0,num+1,2):
+    print(i)
 
 
 # 4.	Count the Number of Digits in a Number
 
-# num = input("enter the number : ")
-# count = len(num)
-# print(count)
+num = input("enter the number : ")
+count = len(num)
+print(count)
 
 # 5.Reverse a Number
 num = input("enter the number : ")
+reverse = "".join(reversed(num))
+print(num)
+
+
+# or
+
+num = input("enter the number : ")
+reverse = num[::-1]
+print(reverse)
 
