c7c23a0d9e273a3802b7ce87d1860e7fcbd69607
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-05 09:56:55 +0530

    rthrth

--- function/largest_odd.py
+++ function/largest_odd.py
@@ -1,3 +1,11 @@
 # crate a function that will return largest odd number with one param
 # that will return largest odd number from num
 
+def largest_odd_number(num):
+    while num!=0:
+        digit = num % 10
+        if digit%2!=0:
+            print(num)
+            break
+        num = num //10
+largest_odd_number(234)
