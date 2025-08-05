84876eeba52ac2966c725de6ac2828e8b65f0bed
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-05 09:33:52 +0530

    enter 2 numbers and compare last digit od num1 > num2

--- function/sumof_two_numbers.py
+++ function/sumof_two_numbers.py
@@ -1,4 +1,16 @@
-def sum_of_numbers(num1,num2):
-    result = num1+num2
-    return result
-print(sum_of_numbers(100,200))
+# def sum_of_numbers(num1,num2):
+#     result = num1+num2
+#     return result
+# print(sum_of_numbers(100,200))
+
+
+# enter 2 numbers and compare last digit od num1 > num2 
+
+def last_digit(num1,num2):
+    last_digit_num1 = num1 % 10
+    last_digit_num2 = num2 % 10
+    if last_digit_num1 > last_digit_num2:
+        return num1
+    else:
+        return num2
+print(last_digit(192,187))
