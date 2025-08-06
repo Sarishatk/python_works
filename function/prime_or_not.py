422eff7a4f32beaace70b2d6de0483dd4a0e6167
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-06 13:38:18 +0530

    # rerurn true if num is palindrome else return not palindrome

--- function/prime_or_not.py
+++ function/prime_or_not.py
@@ -1,12 +1,23 @@
 # craete a function is_prime with one parameter num
 # return true if number is prime else false
 
-def prime_or_not(num):
-    is_prime = True
-    for i in range(2,num):
-        if num%i==0:
-            is_prime = False
-            break
-    return is_prime
-print(prime_or_not(5))
+# def prime_or_not(num):
+#     is_prime = True
+#     for i in range(2,num):
+#         if num%i==0:
+#             is_prime = False
+#             break
+#     return is_prime
+# print(prime_or_not(5))
 
+# create a function is_palindrome with one parameter num
+# rerurn true if num is palindrome else return not palindrome
+
+def is_palindrome(num):
+    original = str(num)
+    reverse = "".join(reversed(str(num)))
+    if original == reverse:
+        return True
+    else:
+        return False
+print(is_palindrome(33))
