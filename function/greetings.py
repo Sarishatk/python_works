0db15ce25ea464022910b08373c8a477fda11cf7
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-06 08:55:40 +0530

    exponent

--- function/greetings.py
+++ function/greetings.py
@@ -4,6 +4,20 @@
 
 
 # divison of 2 numbers
-def num_division(num1=10,num2=12):
-    return num1-num2
-print(num_division(100,56))
+# def num_division(num1=10,num2=12):
+#     return num1-num2
+# print(num_division(100,56))
+
+
+
+
+# def exponent_num(num):
+#     expo = num **2
+#     return (num,expo)
+# print(exponent_num(3))
+
+
+
+def exponent(base,power):
+    return base*power
+print(exponent(2,3))
