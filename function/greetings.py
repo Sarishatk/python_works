57c87de07a156ba60f576ea4a2af07b4a32769f0
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-06 08:47:33 +0530

    # divison of 2 numbers

--- function/greetings.py
+++ function/greetings.py
@@ -1,5 +1,9 @@
-def greetins(name):
-    print("hello ",name)
-greetins("jhon")
+# def greetins(name):
+#     print("hello ",name)
+# greetins("jhon")
 
 
+# divison of 2 numbers
+def num_division(num1=10,num2=12):
+    return num1-num2
+print(num_division(100,56))
