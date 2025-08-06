8cca6b83557fb084f96d34dbd9bb0b5a1923b574
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-06 12:47:09 +0530

    # return true if number is prime else false

--- function/prime_or_not.py
+++ function/prime_or_not.py
@@ -1,3 +1,13 @@
 # craete a function is_prime with one parameter num
 # return true if number is prime else false
 
+def prime_or_not(num):
+ is_prime = True
+ for i in range(2,num):
+    if num%i == 0:
+        is_prime = False
+        break
+ result = "number is prime " if is_prime==True else "number is not prime" 
+ return result
+print(prime_or_not(4))
+
