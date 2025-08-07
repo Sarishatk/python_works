39e96498597883ebfa90e8bb2492739ed63afd97
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-07 09:40:27 +0530

    # print if string contain digit or not

--- string/string_iteration.py
+++ string/string_iteration.py
@@ -5,6 +5,16 @@
     # OR
 
 
-text = "malayalam"
-for ch in text:
-    print(ch)
+# text = "malayalam"
+# for ch in text:
+#     print(ch)
+
+
+
+
+# print if string contain digit or not
+
+word = "helo 0 jhon 28 how are you"
+for ch in word:
+    if ch.isdigit():
+        print(ch)
