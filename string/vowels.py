fe5747dbc63908fde9175242a4d319608f3f073a
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-07 10:22:52 +0530

    # chck the text is pangram or not

--- string/vowels.py
+++ string/vowels.py
@@ -1,13 +1,24 @@
 # read a text from user and display vowel count an consonant count
 
-text = (input("Enter a word : "))
-vowels = "aeiou"
-v_count = 0
-c_count = 0
+# text = (input("Enter a word : ")).casefold()
+# vowels = "aeiou"
+# v_count = 0
+# c_count = 0
+# for ch in text:
+#     if ch in vowels:
+#         v_count+=1
+#     elif ch.isalpha():
+#         c_count+=1
+# print(f"vowel count is : {v_count}")
+# print(f"consonant count is : {c_count}")
+
+
+
+# chck the text is pangram or not
+text = "Quick nymph bugs vex fjord waltz"
+alpha = "abcdefghijklmnopqrstuvwxyz"
+count = 0
 for ch in text:
-    if ch in vowels:
-        v_count+=1
-    else:
-        c_count+=1
-print(f"vowel count id {v_count}")
-print(f"consonant count is {c_count}")
+    if ch in alpha:
+        count+=1
+print(f"total no of pangram in text is : {count}")
