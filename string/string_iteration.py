b106d4e42571ff2dc6dca6fe0aa41d84037621fb
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-07 09:52:33 +0530

    # find the number of vowels count

--- string/string_iteration.py
+++ string/string_iteration.py
@@ -14,7 +14,18 @@
 
 # print if string contain digit or not
 
-word = "helo 0 jhon 28 how are you"
+# word = "helo 0 jhon 28 how are you"
+# for ch in word:
+#     if ch.isdigit():
+#         print(ch)
+
+
+# find the number of vowels count
+
+word = "pneumonoultramicroscopicsilicovolcanoconiosi"
+vowels = "aeiou"
+v_count = 0
 for ch in word:
-    if ch.isdigit():
-        print(ch)
+    if ch in vowels:
+     v_count +=1
+print(v_count)
