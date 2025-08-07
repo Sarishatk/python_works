0aefeedc9507d4bb297d6c688cf204643e582aac
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-07 10:07:38 +0530

    dcw

--- string/vowels.py
+++ string/vowels.py
@@ -1,2 +1,13 @@
 # read a text from user and display vowel count an consonant count
 
+text = (input("Enter a word : "))
+vowels = "aeiou"
+v_count = 0
+c_count = 0
+for ch in text:
+    if ch in vowels:
+        v_count+=1
+    else:
+        c_count+=1
+print(f"vowel count id {v_count}")
+print(f"consonant count is {c_count}")
