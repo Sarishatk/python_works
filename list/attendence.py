b77af3951da18dae474de7a70a89410d8888f247
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-11 10:33:36 +0530

    jhgjh

--- list/attendence.py
+++ list/attendence.py
@@ -4,3 +4,9 @@
 # find total working days
 
 attendance = ["H","P","P","P","P","L","H","H","P","P","P","P","L","H"]
+total_working_day = 0
+for at in attendance:
+    if at != 'H':
+        total_working_day+=1
+print(total_working_day)
+        
