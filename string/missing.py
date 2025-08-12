7461bdd3185ed448211d986be9184a4aac426df0
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-12 10:18:10 +0530

    # find least + ve missing number from this sorted array start from 1

---
+++ string/missing.py
@@ -0,0 +1,9 @@
+# find least + ve missing number from this sorted array start from 1
+
+arr = [1,2,4,5]
+missing_no = 3
+for arr1 in arr:
+    if arr1 == missing_no:
+     print(arr1)
+    else:
+       print(missing_no)
