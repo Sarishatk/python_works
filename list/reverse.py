fa76ba2c1ce961a5d8888b6f1b63bd15b1920ff6
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-12 10:05:03 +0530

    find commom elements in listr

--- list/reverse.py
+++ list/reverse.py
@@ -1,3 +1,16 @@
-num = [1,23,564,4]
-num.sort(reverse=True)
-print(num)
+# num = [1,23,564,4]
+# num.sort(reverse=True)
+# print(num)
+
+
+arr = [1,10,11,12,34,35]
+# create a new array odd_arr, and even_arr
+even_arr = []
+odd_arr = []
+for arr1 in arr:
+    if arr1%2 ==0:
+        even_arr.append(arr1)
+    else:
+        odd_arr.append(arr1)
+print(odd_arr)
+print(even_arr)
---
+++ string/common.py
@@ -0,0 +1,13 @@
+arr = [10,11,20,17,18]
+
+arr2 = [20,10,12,17]
+
+comm_arr = []
+
+for num in arr:
+
+    if num in arr2:
+
+        comm_arr.append(num)
+print(comm_arr)
+ 
