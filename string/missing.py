91392ca12198cdf88aca1c499754e50cb6731f81
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-13 08:52:19 +0530

    solved

--- string/missing.py
+++ string/missing.py
@@ -1,26 +1,34 @@
 # find least + ve missing number from this sorted array start from 1
 
-arr = [1, 2, 4, 5]
+# arr = [1, 2, 4, 5]
 
-missing_no = None
-for i in range(1,len(arr)+2):
-    if i not in arr:
-        missing_no = i
-        break
-print(f"least positive missing number {missing_no}")
+# missing_no = None
+# for i in range(1,len(arr)+2):
+#     if i not in arr:
+#         missing_no = i
+#         break
+# print(f"least positive missing number {missing_no}")
 
 
 # 2nd method of finding least postive missing number
 
 
-arr = [1, 2, 4, 5]
-for i in range(len(arr)-1):
-    j=arr[i]+1
-if arr[i+1] - arr[i] != 1:
-      print("the leat cost positive integer is ",j)
+# arr = [1, 2, 4, 5]
+# for i in range(len(arr)-1):
+#     j=arr[i]+1
+# if arr[i+1] - arr[i] != 1:
+#       print("the leat cost positive integer is ",j)
 
 
 # q3
 
 # arr=[10,11,12,13,11,10,14]
 # display duplicate number without any methods and 'in' operator
+
+arr=[10,11,12,13,11,10,14]
+duplicate_elements = []
+for i in range(len(arr)-1):
+     j=arr[i]+1
+     if arr[i+1]==arr[i]:
+          duplicate_elements.append(i)
+print(duplicate_elements)
