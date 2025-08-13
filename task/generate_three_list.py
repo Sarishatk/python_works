4b60131eaea44fcc472ed8f48edcefa3bb3d7372
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-13 13:08:16 +0530

    armstrong

--- task/generate_three_list.py
+++ task/generate_three_list.py
@@ -2,20 +2,31 @@
  # arr=[153,151,1634,1771,2332]
 
 # generate three new list palindrome_list and armstrong_list,prime_number
-
-arr=[153,151,1634,1771,2332]
+arr = [153, 151, 1634, 1771, 2332]
 palindrome = []
 armstrong = []
 prime = []
+
 for array in arr:
+   
     reverse = str(array)
     reverse2 = "".join(reversed(reverse))
     if reverse2 == reverse:
-       palindrome.append(array)
+        palindrome.append(array)
 
     
+    original = array
+    total = 0
+    temp = array  
+    while temp != 0:
+        digit = temp % 10
+        total += digit ** len(str(original))
+        temp //= 10
+    if total == original:
+        armstrong.append(original)
 
-        
-        
+print("Palindromes:", palindrome)
+print("Armstrong numbers:", armstrong)
+
 
 
