682dc7ac447799be04c74f762358485a830bcdf7
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-14 09:01:35 +0530

    set methods

--- set/set_problem.py
+++ set/set_problem.py
@@ -1,8 +1,8 @@
 st1 = {10,20,10,20,30,40}
 
-st2 = {20,30,30,10,50,60}
+st2 = {20,30,30,10,50,60,40}
 
-inters = st1.difference(st2)
+inters = st2.issuperset(st1)
 
 print(inters)
 
--- task/duplicate.py
+++ task/duplicate.py
@@ -1,3 +1,19 @@
 
 # arr=[10,11,12,13,11,10,14]
 # display duplicate number without any methods and 'in' operator
+
+
+
+arr = [10, 11, 12, 13, 11, 10, 14]
+duplicates = []
+
+# Loop up to len(arr) - 3 so arr[i + 3] is safe to access
+for i in range(len(arr) - 3):
+    if arr[i] == arr[i + 1]:
+        duplicates.append(i)
+    elif arr[i] == arr[i + 2]:
+        duplicates.append(i)
+    elif arr[i] == arr[i + 3]:
+        duplicates.append(i)
+
+print("Indices of duplicates:", duplicates)
