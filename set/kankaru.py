4aedcdd30932c4c4938717cf90c16a78f6becd6c
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-14 10:09:48 +0530

    cwdc

--- set/kankaru.py
+++ set/kankaru.py
@@ -6,7 +6,11 @@ source_word  = "observe"
 target_word = "observ"
 
 kangaru=(set(source_word).intersection(set(target_word)))
+
 if kangaru == set(target_word):
+
     print("it is a kankaru word")
+
 else:
+    
     print("not an kankaru word")
