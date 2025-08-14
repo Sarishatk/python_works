cf0c2f7e57f472c31714037cfe9f0c7914fb6569
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-14 09:18:34 +0530

    cgnc

--- set/set_problem.py
+++ set/set_problem.py
@@ -10,3 +10,11 @@ seta = {12,20,30,40}
 setb = {30,40,50,60}
 symm = seta.symmetric_difference(setb)
 print(symm)
+
+
+sachin_friends = ["rahul","ganguly","yuvi","zaheer","raina","dhoni"]
+dhoni_friends = ["rahul","ganguly","yuvi","raina","kohli","sachin"]
+sachin_friends=set(sachin_friends)
+dhoni_friends=set(dhoni_friends)
+all = sachin_friends.union(dhoni_friends)
+print(all)
