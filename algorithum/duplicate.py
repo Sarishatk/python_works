f9eed9b242b20358292ce050ea0405cf2d095f4a
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-15 10:31:33 +0530

    # Write a program to search for a character in a string and print all positions where it appears.
    # Input: "programming", search for "m".
    
    # Output: [6, 7].

--- algorithum/duplicate.py
+++ algorithum/duplicate.py
@@ -1,7 +1,6 @@
 # Search for a number in a list containing duplicates and return all indexes where it appears.
 # Example: arr = [5, 3, 7, 5, 2, 5], search for 5 → output indexes: [0, 3, 5].
 
-
 arr = [5, 3, 7, 5, 2, 5]
 search_element = 5
 position =[]
@@ -9,3 +8,22 @@ for i in range(len(arr)):
     if arr[i] == search_element:
         position.append(i)
 print(position) 
+
+
+
+
+# Write a program to search for a character in a string and print all positions where it appears.
+
+# Input: "programming", search for "m".
+
+# Output: [6, 7].
+input= "programming"
+search_element = "m"
+position=[]
+for i in range(len(input)):
+    if input[i] == search_element:
+        position.append(i)
+print(position)
+
+
+
