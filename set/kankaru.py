cccf612322a1d150efbe29bcf9b87af811179b20
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-15 11:19:59 +0530

    real way to find kankaru word

--- set/kankaru.py
+++ set/kankaru.py
@@ -1,18 +1,31 @@
 # check the word is kankaru or not
 
 
-source_word  = "hlo"
+# source_word  = "hlo"
 
-target_word = "hlo"
+# target_word = "hlo"
 
-kangaru=(set(source_word).intersection(set(target_word)))
-
-if list(kangaru) == list(target_word):
-
-    print("it is a kankaru word")
-
-else:
-
-    print("not an kankaru word")
-
+# for i in range(len(source_word)):
+#     if source_word[i] == target_word:
+#         print("it is kankaru word")
+#     else:
+#         print("not a kankaru word")
 # it is not a proper progarm beacuse we can put targer word = obserrrvvvvv now it show it is kankaru word beacuse set duplications are not allowed
+
+
+
+# 2nd method
+
+source_word = "CHICKEN"
+target_word = "KEN"
+pos = 0
+for ch in source_word:
+    if ch==target_word[pos]:
+        pos+=1
+    if pos == len(target_word):  # if all target letters are found
+            break
+if pos == len(target_word):
+    print("it is kankaru word")
+else:
+    print("not a kankaru word")
+
