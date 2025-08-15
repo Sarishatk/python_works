8f9eb8fc07f8c70eb74cb38ba0eb0f900dffd9be
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-15 10:26:04 +0530

    solved

--- algorithum/duplicate.py
+++ algorithum/duplicate.py
@@ -2,3 +2,10 @@
 # Example: arr = [5, 3, 7, 5, 2, 5], search for 5 → output indexes: [0, 3, 5].
 
 
+arr = [5, 3, 7, 5, 2, 5]
+search_element = 5
+position =[]
+for i in range(len(arr)):
+    if arr[i] == search_element:
+        position.append(i)
+print(position) 
