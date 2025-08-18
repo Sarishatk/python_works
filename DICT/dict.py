5584ea19903969f0316e8a5bd30731cf1af8396d
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-18 09:31:07 +0530

    dictonary work

---
+++ DICT/dict.py
@@ -0,0 +1,11 @@
+employee = {"id":2000,"name":"sarisha","department":"QA","salary":34000}
+
+# display employee depaetmnet
+
+print(employee["department"])
+
+# upadte employe salary as 45000
+
+employee["salary"] = 4500
+
+print(employee)
