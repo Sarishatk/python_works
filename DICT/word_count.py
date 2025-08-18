a361115f26b142dc032a3085f09c2caa677c01e8
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-18 12:30:22 +0530

    # text = "ABCABB" # Display first recursive character

--- DICT/word_count.py
+++ DICT/word_count.py
@@ -5,5 +5,16 @@ for w in word:
     if w in wc:
         wc[w]+=1
     else:
-        wc[w]=1
+        wc[w]=1  
 print(wc)
+
+
+
+
+
+
+# text = "ABCABB"
+# Display first recursive character
+
+text = "ABCABB"
+word = text.split()
