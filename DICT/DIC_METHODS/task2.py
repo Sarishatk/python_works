b370f47cac6e63255d44918f2dd18d4440a53758
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-19 10:21:38 +0530

    dsf

--- DICT/DIC_METHODS/task2.py
+++ DICT/DIC_METHODS/task2.py
@@ -1,2 +1,5 @@
 # text ="this is a python program to find most recursive word this python is simple"
 # display most frequent word
+
+
+text ="this is a python program to find most recursive word this python is simple"
