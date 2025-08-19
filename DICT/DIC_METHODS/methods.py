62e69a2477ee894975fc2cf70d1f64cceb214b14
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-19 10:17:36 +0530

    rtgbr

--- DICT/DIC_METHODS/methods.py
+++ DICT/DIC_METHODS/methods.py
@@ -36,3 +36,12 @@ char_count ={}
 for ch in set(text):
     char_count[ch] = text.count(ch)
 print(char_count)
+
+max_frequency = 0
+max_frequency_dictionary = {}
+for k,v in char_count.items():
+    if v>max_frequency:
+        max_frequency = v
+        max_frequency_dictionary.clear()
+        max_frequency_dictionary[k]=v
+print(max_frequency_dictionary)
