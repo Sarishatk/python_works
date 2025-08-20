13b6a893d4a279a892211290b69ed68b297dac78
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-20 10:08:42 +0530

    # given an array of numbers find the closet number to zero
    # method1

---
+++ INTERVIEW QUESTIONS/first.py
@@ -0,0 +1,13 @@
+# given an array of numbers find the closet number to zero
+
+
+# method1
+arr = [-2,-3,-4,-1,2,3,4,5,1]
+for num in arr:
+    if num>-2 and num<2:
+        print(f"the closest number is {num}")
+
+
+        
+# method2
+
