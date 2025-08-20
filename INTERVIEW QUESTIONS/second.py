ed9fe8579aa24ca93448c257a14eecc47780a734
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-20 15:01:43 +0530

    solved

--- INTERVIEW QUESTIONS/second.py
+++ INTERVIEW QUESTIONS/second.py
@@ -3,3 +3,9 @@
 # arr = [10, 25, 3, 99, 56, 12]
 
 
+arr = [10, 25, 3, 99, 56, 12]
+largest_num = arr[0]
+for num in arr:
+    if largest_num < num:
+        largest_num = num
+print(f"the largest number is {largest_num}")
