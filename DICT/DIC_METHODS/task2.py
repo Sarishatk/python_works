0426328e8c752b5d5c21cfc9c0448bfff0c458c9
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-20 08:56:09 +0530

    # display most frequent word

--- DICT/DIC_METHODS/task2.py
+++ DICT/DIC_METHODS/task2.py
@@ -4,6 +4,14 @@
 
 text ="this is a python program to find most recursive word this python is simple"
 word_count = {}
+txt=text.split()
+for w in txt:
+    if w in word_count:
+        word_count[w]+=1
+    else:
+        word_count[w]=1
+print(word_count)
 
-
+srt_wc = sorted(word_count,reverse=True,key = word_count.get)
+print(srt_wc)
     
