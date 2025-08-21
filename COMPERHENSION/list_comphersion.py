52f43fe368df527a5232603da2516a1e189e384f
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-21 09:15:39 +0530

    # arr = [2,3,4,56] # find square list and cube list using compershion method

--- COMPERHENSION/list_comphersion.py
+++ COMPERHENSION/list_comphersion.py
@@ -14,5 +14,11 @@ upper_case = [item.upper() for item in fruits]
 print(upper_case)
 
 
+# arr = [2,3,4,56]
+# find square list and cube list using compershion method
 
-# 
+arr = [2,3,4,56]
+cube_list = [num**3 for num in arr ]
+print(cube_list)
+square_list = [num**2 for num in arr]
+print(square_list)
