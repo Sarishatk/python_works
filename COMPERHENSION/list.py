cb2d1aa044002bc51425512acc2530296aa8209d
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-21 10:26:39 +0530

    orders = ["tea","dosa","idli","tea","dosa"] order_counr = {items:orders.count(items) for items in orders} print(order_counr)

--- COMPERHENSION/list.py
+++ COMPERHENSION/list.py
@@ -6,3 +6,7 @@ print(dict)
 
 # orders = ["tea","dosa","idli","tea",dosa]
 # find {item:count}
+
+orders = ["tea","dosa","idli","tea","dosa"]
+order_counr = {items:orders.count(items) for items in orders}
+print(order_counr)
