a57dbbcda578ef8d0c42e17ee357a359c63edaa3
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-21 09:09:53 +0530

    # comperhension

---
+++ COMPERHENSION/list_comphersion.py
@@ -0,0 +1,18 @@
+# comperhension
+# easyest way to create list,set,dictonary  from an iterable
+# syntax
+# [experssion iterable condition]
+
+
+
+         # example
+    #  ------------------
+
+
+fruits =["apple","banana","orange"]
+upper_case = [item.upper() for item in fruits]
+print(upper_case)
+
+
+
+# 
