9eb4acaedce711611e1097f857b6d48a42ef4e05
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-22 12:33:17 +0530

    solved

--- string/missing_two.py
+++ string/missing_two.py
@@ -28,3 +28,15 @@ for i in range(len(arr)-1):
 # (Expected: 3, 5, 8)
 
 
+
+arr = [1, 2, 4, 6, 7, 9] 
+missin_no = []                         
+for i in range(len(arr)-1):
+    j = i+1
+    differnce = arr[j] - arr[i]
+    if differnce!=1:
+        for k in range(1,differnce):
+            missin_no.append(arr[i]+1)
+            
+print(missin_no)
+       
