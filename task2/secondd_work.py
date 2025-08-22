53706ed26e7c1cb78eaf3017a19dc09f20877a48
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-22 17:30:46 +0530

    # Generate First N Prime Numbers (starting from 13) # Input: 5 → Output: 13, 17, 19, 23, 29

--- task2/secondd_work.py
+++ task2/secondd_work.py
@@ -10,3 +10,19 @@ for i in word:
         break
     else:
         reapted_character.add(i)
+
+
+# Generate First N Prime Numbers (starting from 13)
+# Input: 5 → Output: 13, 17, 19, 23, 29
+
+
+
+
+# Find Factorial of a Number
+# Input: 5 → Output: 120
+
+# Check Armstrong Number
+# Input: 153 → Output: True (since 1³+5³+3³=153)
+
+# Find GCD of Two Numbers
+# Input: 48, 18 → Output: 6
