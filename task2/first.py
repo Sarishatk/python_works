6d305c340a40a3eb8ec9a32ab8685d0b847b1668
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-22 14:38:25 +0530

    # Check if a word is palindrom # Input: "madam" → Output: True # Input: "hello" → Output: False

--- task2/first.py
+++ task2/first.py
@@ -1,4 +1,27 @@
-# Count vowels in a word
+#1.  Count vowels in a word
 # Input: "pneumonia" → Output: 5
 
+# h
 
+
+# 2. Count consonants in a word
+# Input: "hello" → Output: 3 (h, l, l)
+
+# input = "hello"
+# vowels = "aeiou"
+# for i in input:
+#     if i not in vowels:
+#      print(i,end=",")
+
+
+
+# Check if a word is palindrom
+# Input: "madam" → Output: True
+# Input: "hello" → Output: False
+
+user_input = input("enter a string: ")
+reverse_d = user_input[::-1]
+if user_input==reverse_d:
+    print("the string is palindrome")
+else:
+    print("the string is not palindrome")
