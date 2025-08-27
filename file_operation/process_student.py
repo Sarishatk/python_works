d8d6505c3b5af99e714304aaa0ca862e866e2346
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-27 10:12:28 +0530

    wefew

---
+++ file_operation/process_student.py
@@ -0,0 +1,2 @@
+passed_path = "C://Users//kumar//OneDrive//Desktop//Python_Works//python_works//file_operation//passed.txt"
+failed_path = "C://Users//kumar//OneDrive//Desktop//Python_Works//python_works//file_operation//failed.txt"
