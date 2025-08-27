1174c1dac105369aecc5d5aa55e3c44550993fd2
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-27 09:52:59 +0530

    palindrom check using file operation

--- file_operation/palindrome.py
+++ file_operation/palindrome.py
@@ -1,2 +1,6 @@
 words = ["hello","hai","madam","racecar","pangaram"]
 file_path = "C://Users//kumar//OneDrive//Desktop//Python_Works//python_works//file_operation//word.txt"
+fw = open(file_path,"w")
+for items in words:
+    if items == items[::-1]:
+        fw.write(items+"\n")
