7df7dda8a1832b2d4373a14a0a8d6ac23cb6571d
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-28 09:18:07 +0530

    file process

---
+++ file_operation/process_employe.py
@@ -0,0 +1,27 @@
+file_path = "C:/Users/kumar/OneDrive/Desktop/Python_Works/python_works/file_operation/employee.csv"
+
+fr = open(file_path,"r")
+
+all_employees  =  []
+
+for line in fr:
+
+    line = line.rstrip('\n')
+
+    data = line.split(",")
+
+    dictionary = {"id" : data[0],"name":data[1],
+                  
+                  "department":data[2],"salary":data[3],
+
+                  "email":data[4],"location":data[5]
+                  
+                   }
+    all_employees.append(dictionary)
+
+print(all_employees)
+
+
+
+
+
