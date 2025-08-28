ba3adbb60df6d78e14556b42f35b784909d3bdae
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-28 10:08:31 +0530

    new set of data

---
+++ file_operation/diet_lens.py
@@ -0,0 +1,21 @@
+path = "C:/Users/kumar/OneDrive/Desktop/Python_Works/python_works/file_operation/food_logs.csv"
+
+fr = open(path,"r")
+
+food_logs =[]
+
+for line in fr:
+
+    data = line.rstrip("\n").split(",")
+
+    if len(data)>1:
+
+        dictionary = {"data":data[0],"meal_type":data[1],
+                    "name":data[2],
+                    "serving_type":data[3]
+                    ,"calories":data[4]
+                    }
+        
+    food_logs.append(dictionary)
+
+print(food_logs)
