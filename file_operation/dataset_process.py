281cd5dfa14535696c8fcf59f2f20d1827a31cf1
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-29 08:34:27 +0530

    list of dictionary created

--- file_operation/dataset_process.py
+++ file_operation/dataset_process.py
@@ -3,4 +3,9 @@ fr = open(file_path,"r")
 dataset = []
 for lines in fr:
     data =   data = lines.rstrip("\n").split(",")
-    
+    dictionary = {"jobId":data[0],"Company Name":data[1],
+                  "Job Title":data[2],"Location":data[3],
+                  "Job Type":data[4],"Salary":data[5],
+                  "Posted Date":data[6]}
+    dataset.append(dictionary)
+print(dataset)
