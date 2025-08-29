579d4eb6aaf1c3f801c72852fc40b3daea31713b
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-29 10:04:44 +0530

    completed

--- OOPS/employee.py
+++ OOPS/employee.py
@@ -39,3 +39,46 @@ student_instance2.display_student()
 
 
 
+
+# super heros [name,movie,year,series type](set_superheros(),display_superheros())
+
+class SuperHero:
+   
+   name:str
+
+   movie:str
+
+   year:int
+
+   seriesType:str
+
+   def set_superheros(self,name,movie,year,seriesType):
+      self.name = name
+
+      self.movie = movie
+
+      self.year = year
+
+      self.seriesType = seriesType
+
+   def display_superheros(self):
+         
+         print(  self.name,self.movie,  self.year,  self.year)
+
+superhero_instamce1 = SuperHero()
+
+superhero_instamce2 = SuperHero()
+
+superhero_instamce1.set_superheros("Iron Man","Iron Man 3",2018, "Avengers series")
+
+superhero_instamce1.display_superheros()
+
+superhero_instamce2.set_superheros("Captain America","Winter Soldier",2004,"Avengers series")
+
+superhero_instamce2.display_superheros()
+
+
+
+
+
+
