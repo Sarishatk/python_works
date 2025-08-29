06c2bfcb7bc8c28cc0884761c49fe8f7183f82d2
Sarisha.tk <sarishakrishna58@gmail.com>
2025-08-29 09:47:16 +0530

    oops

--- OOPS/employee.py
+++ OOPS/employee.py
@@ -1,3 +1,41 @@
 # student (id,name,department ,domain)[set_student(),display_students()]
 
 
+class Student:
+
+    id:int
+
+    name:str
+
+    department:str
+
+    domain:str
+
+    def set_student(self,id,name,department,domain):
+
+        self.id =  id
+
+        self.name = name
+
+        self.department = department
+
+        self.domain = domain
+
+    def display_student(self):
+      
+      print(self.id,self.name,self.department,self.domain)
+
+student_instance1 = Student()
+
+student_instance2 = Student()
+
+student_instance1.set_student(1,"dona","CS","AI")
+
+student_instance1.display_student()
+
+student_instance2.set_student(2,"Anagha","CS","ML")
+
+student_instance2.display_student()
+
+
+
