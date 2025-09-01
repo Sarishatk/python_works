973539949dfcb973db676ddc1ad13ae86331c675
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-01 10:22:46 +0530

    multiple inheritance

--- OOPS/multiple_inheritance.py
+++ OOPS/multiple_inheritance.py
@@ -1,1 +1,25 @@
 # child class inherit multiple class
+
+
+
+class Father:
+
+    def cricklet_skilss(self):
+
+        print("father cricket skill")
+
+class Mother():
+
+    def cooking_skill():
+
+        print("mother cooking skill")
+
+class Child(Father,Mother):
+
+    def play(self):
+
+        print("playing football")
+
+child_instance = Child()
+
+child_instance.cricklet_skilss()
