81bfeea7bd8e265b752ca5b5b1244a397891664f
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-01 10:13:17 +0530

    multilevel finished

--- OOPS/multi_level.py
+++ OOPS/multi_level.py
@@ -1,1 +1,27 @@
-def GrandParent:
+class GrandParent:
+
+    def house(self):
+
+        print("grand parent house")
+
+
+class Parent(GrandParent):
+
+    def car(self):
+
+        print("parent swift car")
+
+class child(Parent):
+
+    def bike(self):
+
+        print("child bike method")
+
+child_instance = child()
+
+child_instance.car()
+
+child_instance.bike()
+
+child_instance.house()
+
