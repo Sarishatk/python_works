226fe57dd19d49b7f568d71440988b646a5f06d2
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-02 09:29:31 +0530

    finished

--- OOPS/inheritance_prblm.py
+++ OOPS/inheritance_prblm.py
@@ -1,26 +1,48 @@
 class Shape:
 
-    def __int__(self,name,edge_count):
+    def __init__(self, name, edge_count):
 
         self.name = name
 
         self.edge_count = edge_count
 
+
     def calculate_area(self):
 
         print(f"calculating {self.name} area")
 
-class Squre(Shape):
-    
-    def __init__(self,name,edge_count,s):
+class Square(Shape):  
 
-        super().__init__(name,edge_count)
-        
+    def __init__(self, name, edge_count, s):
+
+        super().__init__(name, edge_count)
+
         self.s = s
 
     def calculate_area(self):
-        
-        a = self.s**2
+
+        a = self.s ** 2
 
         print(f"area of {self.name} is {a}")
 
+class Rectangle(Shape):
+
+    def __init__(self, name, edge_count, b, h):
+
+        super().__init__(name, edge_count)
+
+        self.b = b
+
+        self.h = h
+
+
+    def calculate_area(self):
+
+        a = self.b * self.h
+
+        print(f"area of {self.name} is {a}")
+
+
+
+sq = Square("Square", 4, 5)
+sq.calculate_area()
