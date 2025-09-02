db29b285e7e7bc9fd3fec9c2731e74133fe1ab0f
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-02 09:23:39 +0530

    area calculated

--- OOPS/inheritance_prblm.py
+++ OOPS/inheritance_prblm.py
@@ -1,4 +1,26 @@
 class Shape:
 
-    def __int__(self,name,edge,edge_count):
+    def __int__(self,name,edge_count):
+
+        self.name = name
+
+        self.edge_count = edge_count
+
+    def calculate_area(self):
+
+        print(f"calculating {self.name} area")
+
+class Squre(Shape):
+    
+    def __init__(self,name,edge_count,s):
+
+        super().__init__(name,edge_count)
+        
+        self.s = s
+
+    def calculate_area(self):
+        
+        a = self.s**2
+
+        print(f"area of {self.name} is {a}")
 
