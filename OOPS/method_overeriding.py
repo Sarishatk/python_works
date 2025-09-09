eb924ea5f545048b83f93cd273d4d396e57a04de
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-09 08:50:30 +0530

    method overriding

---
+++ OOPS/method_overeriding.py
@@ -0,0 +1,27 @@
+class Parent:
+    def vechicle(self):
+        print("passion-pro")
+    
+    def house(self): 
+        print("builining")
+
+class Child(Parent):
+
+    def vehicle(self):
+         print("jawa")
+
+
+
+
+
+
+
+
+
+
+
+"""
+customize behaviour in child class without changing parent class
+
+to provide specific implementation of a method that is already definied in parent classs
+"""
