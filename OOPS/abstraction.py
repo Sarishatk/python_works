c964626c15d1efa97a68a1d805ddb1d125c447cc
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-09 09:54:21 +0530

    example program of abstraction

--- OOPS/abstraction.py
+++ OOPS/abstraction.py
@@ -4,4 +4,10 @@ abstraction:
             
         hiding implimentation details
 
+        ABC :AbstractBaseClass
+
+        decaorator abs class nillu ABC,AbstractMethod ne import cheyyanam
+
+        ennale abstract class methods aavu
+
 """
