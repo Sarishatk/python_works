8d8ac9b28fa57673bb8260d27605ccbddc858d54
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-11 09:44:33 +0530

    fugt

--- ERROR_HANDLING/method.py
+++ ERROR_HANDLING/method.py
@@ -11,3 +11,17 @@ print("send text message to user phone number")
 
 print("send email confirmation")
 
+
+
+
+
+arr = [10,11,12,45,27,18,15]
+
+index = int(input("enter index position > :"))
+
+try:
+    print(arr[index])
+except Exception as e:
+    print(e)
+
+print("program completed")
