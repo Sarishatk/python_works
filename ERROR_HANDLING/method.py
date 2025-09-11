105692c6373721ed7563fadc14eac51496538156
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-11 09:26:39 +0530

    error handling

---
+++ ERROR_HANDLING/method.py
@@ -0,0 +1,13 @@
+num1 = int(input("enter number1"))
+num2= int(input("enter number2"))
+
+try:
+    div_res = num1/num2
+    print(div_res)
+except:
+    print("error occured")
+
+print("send text message to user phone number")
+
+print("send email confirmation")
+
