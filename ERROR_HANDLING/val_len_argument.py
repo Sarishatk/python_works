6f87e3aa9d31979ae4649d5723b067e5b3a13f61
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-12 09:15:38 +0530

    next commit

--- ERROR_HANDLING/val_len_argument.py
+++ ERROR_HANDLING/val_len_argument.py
@@ -1,7 +1,7 @@
 """
-*args
-**kwargs
-
+*args recieve parameter as a tuple
+**kwargs recieve parameter as a dictionary
+ 
 """
 
 def add_numbers(*args):
@@ -10,3 +10,23 @@ def add_numbers(*args):
 
 print(add_numbers(23,40))
 print(add_numbers(10,20,-45))
+
+
+
+def display_person_details(*args):
+    print(args)
+    name = args[0]
+    address = args[1]
+
+    print(name)
+    print(address)
+
+
+display_person_details("james","street1","uk","landon",3532456)
+
+
+def display_person_details(**args):
+
+    print(args)
+
+display_person_details(name = "james",rollno = 3532456, place = "london")
