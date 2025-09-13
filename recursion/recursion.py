dcde87a36fc83215cb707bb66069ca433dffe1f0
Sarisha.tk <sarishakrishna58@gmail.com>
2025-09-13 09:56:51 +0530

    recusrion factorial of a number

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,9 +1,22 @@
 
 
 
-def count(n):
+# def count(n):
+#     if n==0:
+#         return
+#     print(n)
+#     return count(n-1)
+# count(5)
+
+
+
+
+def fact(n):
+
     if n==0:
-        return
-    print(n)
-    return count(n-1)
-count(5)
+
+        return 1
+    
+    return n*fact(n-1)
+
+print(fact(5))
